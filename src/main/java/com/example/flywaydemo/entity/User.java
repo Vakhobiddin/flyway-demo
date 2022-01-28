ba15edb5210e501f64d2955e8a5cc96b124f4f05
Vakhobiddin <vboboqulov@mail.ru>
2022-01-28 12:33:14 +0500

    flyway

---
+++ src/main/java/com/example/flywaydemo/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.flywaydemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 1, max = 100)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    private String lastName;
+}
